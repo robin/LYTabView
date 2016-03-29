0928aa318464b88ec8fbbbfe1d4c757fda04b153
Lu Yibin <robinlu@me.com>
2016-03-29 15:07:25 +0800

    Initial Commit

---
+++ LYTabBarView/LYTabBarView.h
@@ -0,0 +1,19 @@
+//
+//  LYTabBarView.h
+//  LYTabBarView
+//
+//  Created by Lu Yibin on 16/3/29.
+//  Copyright © 2016年 Lu Yibin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LYTabBarView.
+FOUNDATION_EXPORT double LYTabBarViewVersionNumber;
+
+//! Project version string for LYTabBarView.
+FOUNDATION_EXPORT const unsigned char LYTabBarViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LYTabBarView/PublicHeader.h>
+
+
