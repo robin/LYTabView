6328562177eea56dabd56d31da051ef05c5f0463
Lu Yibin <robinlu@me.com>
2017-09-06 17:51:20 +0800

    migrate test

--- LYTabBarViewTests/LYTabBarViewTests.swift
+++ LYTabBarViewTests/LYTabBarViewTests.swift
@@ -7,7 +7,7 @@
 //
 
 import XCTest
-@testable import LYTabBarView
+@testable import LYTabView
 
 class LYTabBarViewTests: XCTestCase {
 
@@ -28,7 +28,7 @@ class LYTabBarViewTests: XCTestCase {
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
